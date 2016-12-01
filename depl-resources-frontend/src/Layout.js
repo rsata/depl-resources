3870971a7d32f99b1eecb935a9d482860ab891bc
Reid Sata <rsata@rubiconnt.localnet>
2016-12-01 11:21:48 -0800

    remove deployment title

--- depl-resources-frontend/src/Layout.js
+++ depl-resources-frontend/src/Layout.js
@@ -1,17 +1,15 @@
 // import { bindActionCreators } from 'redux';
 import React, { Component } from 'react';
-import { Link } from 'react-router'
+// import { Link } from 'react-router'
 import './main.css';
+
+import { Nav } from './components/nav/Nav.js';
 
 export default class Layout extends Component{
   render() {
     return (
       <div>
-        <h1><Link to="/">BLAAAA</Link></h1>
-        <ul>
-          <li><Link to="/deployment">deployment</Link></li>
-          <li><Link to="/heat">heat</Link></li>
-        </ul>
+        <Nav />
 
         {/* We use cloneElement here so we can auto pass down props */}
         {/* Essentially each element is getting cloned with its own props so children of it can call this.props */}
