901581085f1201cce86314401a4d85076323b212
Reid Sata <rsata@rubiconnt.localnet>
2016-12-12 12:58:07 -0800

    footer placeholder

--- depl-resources-frontend/src/Layout.js
+++ depl-resources-frontend/src/Layout.js
@@ -1,9 +1,9 @@
 // import { bindActionCreators } from 'redux';
 import React, { Component } from 'react';
-// import { Link } from 'react-router'
 import './main.css';
 
 import { Nav } from './components/nav/Nav';
+import { Footer } from './components/footer/Footer';
 import { SearchExternal } from './components/searchExternal/SearchExternal';
 
 const styles = {
@@ -24,6 +24,9 @@ export default class Layout extends Component{
           {/* We use cloneElement here so we can auto pass down props */}
           {/* Essentially each element is getting cloned with its own props so children of it can call this.props */}
           { React.cloneElement(this.props.children, this.props) }
+        </div>
+        <div>
+          <Footer />
         </div>
       </div>
     );
---
+++ depl-resources-frontend/src/components/footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Footer = (props) => {
+  return (
+    <div className='footer'>
+      <h1>Footer</h1>
+    </div>
+  )
+}
