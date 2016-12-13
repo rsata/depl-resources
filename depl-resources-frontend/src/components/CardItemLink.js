626f6c83b85e24d75a623d64b9923f86d6b30416
Reid Sata <rsata@rubiconnt.localnet>
2016-12-13 12:46:49 -0800

    open external links in new tab

--- depl-resources-frontend/src/components/CardItemLink.js
+++ depl-resources-frontend/src/components/CardItemLink.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const CardItemLink = (props) => {
   return (
     <div>
-      <a href={props.url}>{props.title}</a>
+      <a href={props.url} target='_blank'>{props.title}</a>
     </div>
   )
 }
