150660bf01cfcf201165116eb30bda98cc5dcf33
Reid Sata <rsata@rubiconnt.localnet>
2016-12-13 17:19:05 -0800

    fix case, remove react-toolkit

---
+++ depl-resources-frontend/src/containers/Heat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Heat extends React.Component {
+  render() {
+    console.log(this.props)
+    return(
+      <div>
+        Heat page
+      </div>
+    )
+  }
+}
