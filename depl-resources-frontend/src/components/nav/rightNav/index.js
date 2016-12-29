5c0875a006ff4dc69ebaaa771be11ef5a6532aa7
rsata <reid.t.sata@gmail.com>
2016-12-29 11:17:46 -0800

    right nav stuff

--- depl-resources-frontend/src/components/nav/rightNav/index.js
+++ depl-resources-frontend/src/components/nav/rightNav/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import CSSModules from 'react-css-modules';
+import styles from './rightNav.scss';
 
-export default class RightNav extends React.Component {
-  render() {
-    return (
-      <div>
-        hey
-      </div>
-    );
-  }
-}
+const RightNav = (props) => {
+  return (
+    <div styleName='wrapper'>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default CSSModules(RightNav, styles);
