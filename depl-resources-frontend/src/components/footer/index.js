8d4e22f32ce165369be1d619991deea74ba76805
Reid Sata <rsata@rubiconnt.localnet>
2017-01-03 09:11:58 -0800

    footer layout

--- depl-resources-frontend/src/components/footer/index.js
+++ depl-resources-frontend/src/components/footer/index.js
@@ -9,6 +9,7 @@ const Footer = (props) => {
         <li><a href='https://fs.rubicon.com/Rubicon%20Underground/Default.htm' target='_blank'>Facebook</a></li>
         <li><a href='https://fs.rubicon.com/Rubicon%20Underground/Default.htm#BuckinghamPalace/Phone List.htm%3FTocPath%3DBuckingham%2520Palace%7C_____5' target='_blank'>Phone List</a></li>
         <li>Channel Map</li>
+        <li>Onboarding</li>
       </ul>
     </div>
   );
