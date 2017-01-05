ce954c860e7f684a01eb44b4979abc00d81bb9f8
Reid Sata <rsata@rubiconnt.localnet>
2017-01-05 09:40:16 -0800

    add notification on copy

--- depl-resources-frontend/src/components/nav/Password.js
+++ depl-resources-frontend/src/components/nav/Password.js
@@ -4,16 +4,36 @@ import ContentCopy from 'react-icons/lib/md/content-copy';
 // sorry...
 const styles = {
   position: 'fixed',
-  marginTop: '-500px'
+  marginTop: '-500px',
+  copied: {
+    position: 'fixed',
+    backgroundColor: '#555',
+    margin: '10px',
+    padding: '10px',
+    zIndex: '99'
+  }
 };
 
 export class Password extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      copied: false
+    };
+  }
+
   copyPw(e) {
     e.preventDefault();
     document.querySelector('#pw').select();
-    var lag = window.setTimeout(
-      document.execCommand('copy')
-    , 500);
+    let copy = window.setTimeout(() => {
+      document.execCommand('copy');
+      this.setState({copied: !this.state.copied});
+    }
+    , 100);
+    let hide = window.setTimeout(() => {
+      this.setState({copied: !this.state.copied});
+    }
+    , 2000);
   }
 
   render() {
@@ -21,7 +41,14 @@ export class Password extends React.Component {
       <li>
         <div onClick={this.copyPw.bind(this)}>{this.props.password}&nbsp;&nbsp;<ContentCopy /></div>
         <div><input id='pw' defaultValue={this.props.password} style={styles} /></div>
+        {this.state.copied === true ? <Copied /> : null}
       </li>
     );
   }
+};
+
+const Copied = (props) => {
+  return (
+    <div style={styles.copied}>Copied to clipboard!</div>
+  );
 };
